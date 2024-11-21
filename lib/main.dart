bb609078708adc000ecbdf16c543f7106786cdcf
hs-2171322-Imhanle <2171322@hansung.ac.kr>
2024-11-21 18:02:45 +0900

    update sign

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'auth.dart';
+import 'sign.dart';
 import 'creation.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-    home: new LoginWidget(),
+    home: new SignWidget(),
   ));
 }
 
