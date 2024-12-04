581cd3458f8e37f2afe06dbc002bc2a8b1c8ce0b
FriedKimBap <rladudals4356@gmail.com>
2024-12-04 17:06:27 +0900

    찐찐찐막

--- lib/auth.dart
+++ lib/auth.dart
@@ -81,6 +81,7 @@ class LoginWidget extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.only(left: 30.0, top: 40.0),
           child: AppBar(
+            scrolledUnderElevation: 0,
             backgroundColor: Colors.white,
             leading: IconButton(
               onPressed: () {
@@ -257,8 +258,8 @@ class ChangePwWidget extends StatelessWidget { // 로그인 화면
         child: Padding(
           padding: EdgeInsets.only(left: 30.0, top: 40.0),
           child: AppBar(
-            backgroundColor: Colors.white,
             scrolledUnderElevation: 0,
+            backgroundColor: Colors.white,
             leading: IconButton(
               onPressed: () {
                 Navigator.pop(context);
